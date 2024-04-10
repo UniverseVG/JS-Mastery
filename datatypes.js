955163304985ba7e05ab0f3da345fe8ff1042c66
UniverseVG <gmvarun2001@gmail.com>
2024-04-10 12:04:43 +0530

    add datatype file

---
+++ datatypes.js
@@ -0,0 +1,20 @@
+/*
+  primitive data types
+
+  number => 2^52
+  string => ""
+  boolean => true/false
+  null => standalone value
+  undefined => variable without value
+  symbol => unique identifier
+  bigint => number > 2^53
+
+  Symbols in JavaScript are a primitive data type introduced in ECMAScript 6 (ES6). They provide a way to create unique identifiers that can be used as property keys in objects. Unlike strings or numbers, symbols are guaranteed to be unique, which helps avoid naming collisions when working with objects.
+
+   
+
+  type of null is object and undefined is undefined
+
+  
+
+*/
